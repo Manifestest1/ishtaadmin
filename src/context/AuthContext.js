452806d17238e1cb48cc1e9cc63c6bc74acac982
nileshji <manifestest1@gmail.com>
2024-06-14 17:39:14 +0530

    Add Admin

---
+++ src/context/AuthContext.js
@@ -0,0 +1,60 @@
+import { createContext, useState, useEffect } from 'react';
+import axios from '../utils/axios';  // Import the configured Axios instance
+import { useRouter } from 'next/router';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => { 
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const router = useRouter();
+
+    useEffect(() => {
+        const token = localStorage.getItem('token'); 
+        if (token) 
+        {
+            axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+            axios.get('/admin_user')
+                .then(response => {
+                    setUser(response.data);
+                    console.log(response,"Check Api");
+                    setLoading(false);
+                    // router.push('/dashboard');
+                })
+                .catch(() => {
+                    localStorage.removeItem('token');
+                    setLoading(false);
+                });
+        } 
+        else 
+        {
+            setLoading(false);
+        }
+    }, []);
+
+    const login = async (email, password) => {
+        const response = await axios.post('/admin_login', { email, password });
+        console.log(response,"Check Api");
+        const token = response.data.authorisation;
+        localStorage.setItem('token', token);
+        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+        setUser(response.data.admin);
+        router.push('/dashboard');
+    };
+
+    const logout = async () => {
+        const response = await axios.post('/logout');
+        console.log(response,"Check Logout Api");
+        localStorage.removeItem('token');
+        setUser(null);
+        router.push('/');
+    };
+
+    return (
+        <AuthContext.Provider value={{ user, loading, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthContext;
